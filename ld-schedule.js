867f661e8c5d9192f8fb6d9f0ead2547f4a28419
fallingpizza11 <fallingpizza11@gmail.com>
2020-07-04 23:06:13 -0700

    break stuff

--- ld-schedule.js
+++ ld-schedule.js
@@ -6,20 +6,49 @@ console.log('script has been loaded!')
 
 // constants for the name of the classes / selectors in case they change
 const frame_name = '.krn-widget-iframe'
-const week_name = '.fc-week'
-const time_container_name = '.fc-event-container'
+const shift_event_name = 'shift-event'
+const month_btn_id = 'employeecalendar.actions.switchViewToMonthly_btn'
 
 // selects the iframe, then selects the html contained inside the iframe
 const html_frame = document.querySelector(frame_name)
+
+/** @type {HTMLElement} */
 const frame_doc = html_frame.contentDocument
 
-let time_containers = frame_doc.querySelectorAll(time_container_name)
-//todo: remove everything from time_containers that dosen't have a child a tag with the class name '.shift-event'
-//      this is because an event-container can also be for your vacation requests too
+const month_btn = frame_doc.getElementById(month_btn_id)
 
-for (let i = 0; i < time_containers.length; i++) {
-    console.log('container' + i + 'has an index of: ' + time_containers[i].cellIndex)
+/**
+ * callback function that runs when an observation is made on an Element
+ * @param {MutationRecord[]} mutations - list of mutations?
+ * @param {*} observer - dictionary for observer config
+ */
+const attrChanged = function(mutations, observer) {
+    console.log('something changed...')
+    
+    for(let mutation of mutations) {
+        if(mutation.type === 'attributes') {
+            console.log('an observation has been made on : ' + mutation.attributeName)
+        }
+    }
 }
+
+const observer = new MutationObserver( () => {console.log('Something has changed')} )
+observer.observe(month_btn, {attributes: true, childList: true, subtree: true})
+console.log('waiting for change...');
+
+//todo: no idea why this is not working should trigger when the class changes
+
+// month_btn.click()
+
+
+
+// let shift_events = frame_doc.getElementsByClassName(shift_event_name)
+
+
+
+// for (let i = 0; i < shift_events.length; i++) {
+//     console.log('container' + i + 'has an index of: ' + shift_events[i].cellIndex)
+// }
 
 
 //todo: run script when clicked from the addon bar - DONE
