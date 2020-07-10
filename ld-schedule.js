5fa7413fa8a1e8a08a2a872911b1e5bb3e0a11bb
fallingpizza11 <fallingpizza11@gmail.com>
2020-07-10 00:47:10 -0700

    progress

--- ld-schedule.js
+++ ld-schedule.js
@@ -6,24 +6,49 @@ console.log('script has been loaded!')
 
 // constants for the name of the classes / selectors in case they change
 const frame_name = '.krn-widget-iframe'
-const shift_event_name = 'shift-event'
+const shift_event_class = 'shift-event'
 const month_btn_id = 'employeecalendar.actions.switchViewToMonthly'
+const dropdown_btn_name = '#timeframe-selector-input'
+const dropdown_name = '.dropdown-menu'
 
 // selects the iframe, then selects the html contained inside the iframe
 const html_frame = document.querySelector(frame_name)
 
-/** @type {HTMLElement} */
+/** @type {HTMLDocument} */
 const frame_doc = html_frame.contentDocument
-
 const month_btn = frame_doc.getElementById(month_btn_id)
+console.log('Seting up observer..') 
 
-
-const observer = new MutationObserver( () => {console.log('Something has changed')} )
+// sets up an observer to wait for changes to the month_btn element
+const observer = new MutationObserver(selectSchedulePeriod)
+//todo: this will run the function every time
 observer.observe(month_btn, {attributes: true, childList: true, subtree: true})
 console.log('waiting for change...') 
 
-
 month_btn.click()
+console.log('click')
+
+
+/** 
+* @param {MutationObserver} observer 
+*/
+function selectSchedulePeriod(observer) {
+    console.log('‼something has changed‼');
+    //todo: this does not disconnect the observer
+    observer.disconnect
+    /** @type {HTMLElement} */
+    const dropdown = frame_doc.querySelector(dropdown_name)
+    /** @type {HTMLElement} */
+    const dropdown_btn = frame_doc.querySelector(dropdown_btn_name)
+    dropdown_btn.click()
+    console.log('click')
+    
+
+    const currentSchedule = dropdown.children[4]
+    currentSchedule.click()
+    console.log('click')
+
+}
 
 
 
