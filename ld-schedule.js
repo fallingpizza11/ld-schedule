cf54b5ab9b0fbee55fe8c5cce35350fca21b54c8
fallingpizza11 <fallingpizza11@gmail.com>
2020-07-18 20:04:26 -0700

    messing with observation

--- ld-schedule.js
+++ ld-schedule.js
@@ -1,12 +1,15 @@
 // ld-shedule.js
 // semicolons are for nerds
 
+
 console.log('script has been loaded!')
 
+const week_view_name = '.fc-agendaWeek-view'
+const month_view_name = '.fc-week-view'
 
 // constants for the name of the classes / selectors in case they change
 const frame_name = '.krn-widget-iframe'
-const shift_event_class = 'shift-event'
+const shift_event_name = '.shift-event'
 const month_btn_id = 'employeecalendar.actions.switchViewToMonthly'
 const dropdown_btn_name = '#timeframe-selector-input'
 const dropdown_name = '.dropdown-menu'
@@ -20,12 +23,32 @@ const month_btn = frame_doc.getElementById(month_btn_id)
 console.log('Seting up observer..') 
 
 // sets up an observer to wait for changes to the month_btn element
-const observer = new MutationObserver(selectSchedulePeriod)
-observer.observe(month_btn, {attributes: true, childList: true, subtree: true})
+// const observer = new MutationObserver(selectSchedulePeriod)
+// observer.observe(month_btn, {attributes: true, childList: true, subtree: true})
+// console.log('waiting for change...') 
+
+const fc_view = frame_doc.querySelector(week_view_name)
+
+//observer for the class on the fc-view element to change
+const observer = new MutationObserver(domChanged)
+observer.observe(fc_view, {attributes: true})
 console.log('waiting for change...') 
 
 month_btn.click()
 console.log('click')
+
+function domChanged(mutations, observer) {
+    console.log('‼view is in view‼');
+
+    const shift_events = frame_doc.querySelectorAll(shift_event_name)
+
+    for (let i = 0; i < shift_events.length; i++) {
+        console.log('container' + i + 'has an index of: ' + shift_events[i].cellIndex)
+    }
+
+    observer.disconnect()
+}
+
 
 
 /**
@@ -50,17 +73,21 @@ function selectSchedulePeriod(mutations, observer) {
 
     //todo: figure out how to escape function, continuing this thread
     //      without making this function super long and stuff
+
+    const shift_events = frame_doc.querySelectorAll(shift_event_name)
+    console.log(shift_events)
+
+    for (let i = 0; i < shift_events.length; i++) {
+        console.log('container' + i + 'has an index of: ' + shift_events[i].cellIndex)
+    }
+
 }
 
 
 
-// let shift_events = frame_doc.getElementsByClassName(shift_event_name)
 
 
 
-// for (let i = 0; i < shift_events.length; i++) {
-//     console.log('container' + i + 'has an index of: ' + shift_events[i].cellIndex)
-// }
 
 
 //todo: run script when clicked from the addon bar - DONE
