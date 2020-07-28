a256018ac37ad03773ea746b6a15ba5324a2ab8b
Isaac <fallingpizza11@gmail.com>
2020-07-27 23:33:17 -0700

    rip observer :(

--- ld-schedule.js
+++ ld-schedule.js
@@ -6,7 +6,7 @@ console.log('script has been loaded!')
 
 
 const week_view_name = '.fc-agendaWeek-view'
-const month_view_name = '.fc-week-view'
+const month_view_name = '.fc-month-view'
 
 // constants for the name of the classes / selectors in case they change
 const frame_name = '.krn-widget-iframe'
@@ -33,91 +33,68 @@ console.log('Seting up observer..')
 
 const fc_view = frame_doc.querySelector(week_view_name)
 
-
 /** @type {HTMLElement} */
 const dropdown = frame_doc.querySelector(dropdown_name)
 /** @type {HTMLElement} */
 const dropdown_btn = frame_doc.querySelector(dropdown_btn_name)
+
 dropdown_btn.click()
 console.log('click 1')
-const currentSchedule = dropdown.children[4]
-currentSchedule.click()
+
+dropdown.children[4].click()
 console.log('click 2')
 
 month_btn.click()
 console.log('click 3')
 
-//observer for the class on the fc-view element to change
-const observer = new MutationObserver(domChanged)
-observer.observe(fc_view, {attributes: true})
-console.log('waiting for change...') 
-
-
-function domChanged(mutations, observer) {
-
-    /** @type {HTMLElement} maybe not const tho */
-    const content_skeleton = frame_doc.querySelector(skeleton_name)
-    /** @type {HTMLElement} maybe not const tho */
-    const event_container = frame_doc.querySelector(container_name)
-
-    console.log('‼view is in view‼');
-
-    //this if statement is useless, it dosent work, need to find another way to only run that code when the element actually loads
-
-
-    if(!(frame_doc.querySelector(container_name) === null)) {
-
-        const shift_events = frame_doc.querySelectorAll(shift_event_name)
-
-
-        for (let i = 0; i < shift_events.length; i++) {
-    
-            let pos = shift_events[i].parentElement.cellIndex
-            /** @type {HTMLTableElement} */
-            let dateTable = shift_events[i].parentElement.parentElement.parentElement.previousSibling
-            /** @type {HTMLTableCellElement} */
-            let dateCell = dateTable.firstChild.cells[pos]
-    
-            let date = dateCell.getAttribute('data-date')
-    
-            let time = shift_events[i].querySelector('.event-title').innerHTML
-            console.log('you work ' + time + ' on ' + date)
-        }
-    
-        observer.disconnect()
-    }
-}
+// todo: should add an alert to the user when this happens
+waitForKronos().then(getSchedule).catch(e => {
+    console.error(e)
+    window.alert(e)
+})
 
 
 
-/**
-* @param {MutationRecord} mutations
-* @param {MutationObserver} observer 
-*/
-function selectSchedulePeriod(mutations, observer) {
-    console.log('‼something has changed‼');
 
-    observer.disconnect()
-    // :^)
-
-    //todo: figure out how to escape function, continuing this thread
-    //      without making this function super long and stuff
+function getSchedule() {
+    console.log('grabbing shift times...');
 
     const shift_events = frame_doc.querySelectorAll(shift_event_name)
-    console.log(shift_events)
-
     for (let i = 0; i < shift_events.length; i++) {
-        console.log('container' + i + 'has an index of: ' + shift_events[i].cellIndex)
-    }
 
+        let pos = shift_events[i].parentElement.cellIndex
+        /** @type {HTMLTableElement} */
+        let dateTable = shift_events[i].parentElement.parentElement.parentElement.previousSibling
+        /** @type {HTMLTableCellElement} */
+        let dateCell = dateTable.firstChild.cells[pos]
+
+        let date = dateCell.getAttribute('data-date')
+
+        let time = shift_events[i].querySelector('.event-title').innerHTML
+        console.log('you work ' + time + ' on ' + date)
+    }
 }
 
+async function waitForKronos() {
 
+    var count = 9
 
+    do {
 
+    console.log('sleeping...');
 
+    // high class JS version of sleep
+    await new Promise(r => {setTimeout(r , 10)})
 
+    console.log('awake!');
+    
+    count++
+    if (count >= 10) {
+        throw new Error('Kronos failed to load the page, please exit and try again')
+    }
 
+    } while (frame_doc.querySelector(month_view_name) === null || frame_doc.querySelector(container_name) === null);
+}
 
 //todo: run script when clicked from the addon bar - DONE
 //      maybe also be able to have an automatic mode so you can switch to timed capture
